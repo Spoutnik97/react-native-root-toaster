17a2038976ef8337aed2068645d9ffed42d57824
spoutnik97 <guillaume.piedigrossi@gadz.org>
2020-03-06 14:30:44 +0100

    :white_check_mark: (RootToaster) add tests for RootToaster

--- src/__tests__/RootToaster.test.tsx
+++ src/__tests__/RootToaster.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-native-testing-library';
-import { RootToaster } from './RootToaster';
+import { RootToaster } from '../RootToaster';
 
 jest.useFakeTimers();
 describe('RootToaster', () => {
